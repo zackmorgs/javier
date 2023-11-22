a5fd9701b186aeaed750d36713ce41132b693c3d
zackmorgs <zackmorgenthaler@gmail.com>
2023-11-22 14:33:32 -0600

    made stars element work with number of stars.

--- components/reviews-homepage.tsx
+++ components/reviews-homepage.tsx
@@ -6,15 +6,15 @@ export default function ReviewsHomepage() {
         <section id="reviews-homepage">
             <div className="max-w-6xl mx-auto px-5 sm:px-6 py-3 mb-4">
                 <h2 className="text-3xl text-center mt-2 mb-6 font-bold">Reviews</h2>
-                <ul>
+                <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
                     {reviews.map((review) => {
                         return (
                             <li className="review mb-4">
-                                <div className="flex flex-col">
+                                <div className="flex flex-row justify-between">
                                     <b>{review.name}</b>
                                     <Stars stars={review.stars} />
                                 </div>
-                                <p>{review.text}</p>
+                                <p className="text-gray-600">{review.text}</p>
                             </li>
                         )
                     })}
--- components/ui/stars.tsx
+++ components/ui/stars.tsx
@@ -1,14 +1,20 @@
-import { FiStar } from "react-icons/fi";
+import { FaStar } from "react-icons/fa6";
 
 interface StarsProps {
     stars: Number;
 }
 
 export default function Stars({stars}:StarsProps){
+    // Create an array of stars based on the stars prop
+    // thanks chatgpt
+    const starElements = [];
+    for (let i = 0; i < stars; i++) {
+        starElements.push(<FaStar key={i} />);
+    }
     
     return (
         <span className="flex flex-row stars" aria-label="Rated 5.0 out of 5">
-            <FiStar/><FiStar/><FiStar/><FiStar/><FiStar/>
+            {starElements}
         </span>
     )
 }
