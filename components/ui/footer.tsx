814298c50205450d186691885a58754f0f85616d
zackmorgs <zackmorgenthaler@gmail.com>
2023-11-22 16:08:23 -0600

    updated facebook link in footer

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -141,7 +141,7 @@ export default function Footer() {
               </Link>
             </li> */}
             <li className="ml-4">
-              <Link href="https://www.facebook.com/apportal.io/" className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Facebook">
+              <Link href="https://www.facebook.com/p/Javiers-Barbershop-100066316638906/" className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Facebook">
                 <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                   <path d="M14.023 24L14 17h-3v-3h3v-2c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V14H21l-1 3h-2.72v7h-3.257z" />
                 </svg>
