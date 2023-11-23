b48a4312bc789f073c36d78ca921ba29473d832d
zackmorgs <zackmorgenthaler@gmail.com>
2023-11-23 04:40:37 -0600

    footer width

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -16,7 +16,7 @@ export default function Footer() {
 
   return (
     <footer id="footer">
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+      <div className="xl:max-w-7xl mx-auto px-4 sm:px-6">
 
         {/* Top area: Blocks */}
         <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-gray-200">
