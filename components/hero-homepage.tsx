1dbb6bcbcfd3cd5e7c88e306676dda13f8eb88cf
zackmorgs <zackmorgenthaler@gmail.com>
2023-11-23 07:29:45 -0600

    updated fonts

--- components/hero-homepage.tsx
+++ components/hero-homepage.tsx
@@ -12,13 +12,13 @@ export default function HeroHomepage() {
                 </video> */}
             </div>
             <div id="hero-text" className="max-w-6xl mx-auto px-4 sm:px-6">
-                <div className="pt-32 pb-8 md:pt-40 md:pb-20">
+                <div className=" pt-32 pb-8 md:pt-40 md:pb-20">
                     <div className="text-center pb-12 md:pb-16">
                         <div className="mb-4">
                             <TitleXL>Javier's Barbershop</TitleXL>
                         </div>
                         <div className="max-w-xs mx-auto sm:max-w-none text-center" data-aos="zoom-y-out" data-aos-delay="300">
-                            <p>A Barbershop located in Rochester, MN</p>
+                            <p>A barbershop located in Rochester, MN</p>
                             <div className="flex flex-col md:flex md:flex-row lg:flex-row justify-center mt-6">
                                 <Link href="/book-appointment" className="btn btn-primary md:mr-4">
                                     <FiCalendar className="icon inline-block" /> Book an Appointment
--- components/ui/title.tsx
+++ components/ui/title.tsx
@@ -1,28 +1,40 @@
 import React, { ReactNode } from 'react';
+import { Rye, Lora } from 'next/font/google';
+
+const rye = Rye({
+    subsets: ['latin'],
+    weight: ['400']
+});
+
+const lora = Lora({
+    subsets: ['latin'],
+    weight: ['400','500','600', '700']
+})
+
 
 interface TitleProps {
     children: ReactNode;
 }
 
-export function TitleXL({children}:TitleProps) {
+export function TitleXL({ children }: TitleProps) {
     return (
-        <h1 className="text-5xl md:text-6xl font-bold leading-tighter tracking-tighter">{children}</h1>
+        <h1 className={`${rye.className} uppercase text-4xl md:text-6xl font-bold leading-tighter tracking-tighter`}>{children}</h1>
     )
 }
 
-export function TitleLg({children}:TitleProps) {
+export function TitleLg({ children }: TitleProps) {
     return (
-        <h2 className="text-3xl mt-2 font-bold">{children}</h2>
+        <h2 className={`uppercase text-3xl mt-2 font-bold`}>{children}</h2>
     )
 }
 
-export function TitleMd({children}:TitleProps) {
+export function TitleMd({ children }: TitleProps) {
     return (
         <h3 className="font-bold text-2xl">{children}</h3>
     )
 }
 
-export function TitleSm({children}:TitleProps) {
+export function TitleSm({ children }: TitleProps) {
     return (
         <h4 className="text-xl font-bold">{children}</h4>
     )
