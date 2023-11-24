cca739a7b586181f0feb7277e1eff1860f290ff0
zackmorgs <zackmorgenthaler@gmail.com>
2023-11-24 09:40:43 -0600

    some stuff in about-barbershop, build engines test

--- app/about/about-barbershop.tsx
+++ app/about/about-barbershop.tsx
@@ -4,7 +4,7 @@ export default function AboutBarbershop() {
     return (
         <section id="about-barbershop">
             <div className="xl:max-w-7xl mx-auto px-5 sm:px-6 py-3">
-                <div className="mb-3">
+                <div className="mb-4 border-b border-gray-300 pb-4">
                     <TitleMd>The Barbershop</TitleMd>
                 </div>
                 <p className="mb-3">
@@ -13,10 +13,15 @@ export default function AboutBarbershop() {
                 <p className="mb-3">
                     It began gaining great popularity and a reputation through excellent word-of-mouth reviews.
                 </p>
-                <h3 className="text-xl font-bold mb-1">Address</h3>
-                <address>
-                    1112 9th St NE Rochester, MN 55906 
-                </address>
+                <p>The barbershop is </p>
+                <div>
+                    <div className="mb-4 border-b border-gray-300 pb-4">
+                        <TitleMd>Address</TitleMd>
+                    </div>
+                    <address>
+                        1112 9th St NE Rochester, MN 55906
+                    </address>
+                </div>
             </div>
         </section>
     )
