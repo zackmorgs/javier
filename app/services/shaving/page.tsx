56ff11039908de1262e6a0491de849f528de6c0c
zackmorgs <zackmorgenthaler@gmail.com>
2023-11-24 12:08:19 -0600

    updated shaving

--- app/services/shaving/page.tsx
+++ app/services/shaving/page.tsx
@@ -1,7 +1,9 @@
-import Image from 'next/image';
+import Image from "next/image";
 import Hero from "@/components/ui/hero"
+import { TitleMd } from "@/components/ui/title";
 import Gallery from '@/components/ui/gallery';
 import Cta from "@/components/cta"
+
 
 export default function Page() {
     const images = [
@@ -34,10 +36,36 @@ export default function Page() {
     return (
         <>
             <Hero title="Shaving">
-                <p></p>
+                <p>Experience precision and comfort in every shave.</p>
             </Hero>
-            
-            <Gallery title="Examples" className="shaving" galleryImages={images} />
+            <section id="about-shaving">
+                <div className="xl:max-w-6xl mx-auto py-3 block md:flex md:flex-row">
+                    <div className="px-5 sm:px-6 w-1/3">
+                        <Image src="/img/services/straight-razor.jpg" alt="Straight Razor for Shaving" width={375} height={375} />
+                    </div>
+                    <div className="px-5 sm:px-6 w-2/3">
+                        <div className="border-b border-gray-300 pb-2 mb-4 inline-block">
+                            <TitleMd>
+                                About Shaving
+                            </TitleMd>
+                        </div>
+                        <p className="mb-3">
+                            With two decades of expertise in men's grooming, I have mastered the art of the perfect shave. Whether you prefer the classic touch of a straight razor or the modern efficiency of electronic razors, I ensure a close, comfortable shave every time.
+                        </p>
+                        <p className="mb-3">
+                            The straight razor shave is an age-old tradition, celebrated for its precision and the unique experience it offers. It's not just about removing hair; it's a ritual that leaves your skin feeling exceptionally smooth and rejuvenated.
+                        </p>
+                        <p className="mb-3">
+                            For those who seek a quick yet thorough shave, my collection of high-quality electronic razors comes into play. These tools are perfect for a fast-paced lifestyle, offering a consistently close shave with minimal skin irritation.
+                        </p>
+                        <p className="mb-3">
+                            I take pride in tailoring each shave to the individual needs of my clients, ensuring your comfort and satisfaction. Step into my barbershop for a shave that's not just about looking good, but feeling great too.
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <Gallery title="Shaving Examples" className="shaving" galleryImages={images} />
+            <Cta />
         </>
     )
 }
