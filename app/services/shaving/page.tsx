f018cece082c0126712baad9a453ba8375cb7d62
zackmorgs <zackmorgenthaler@gmail.com>
2023-11-24 11:56:22 -0600

    updated shaving

--- app/services/shaving/page.tsx
+++ app/services/shaving/page.tsx
@@ -1,9 +1,43 @@
+import Image from 'next/image';
 import Hero from "@/components/ui/hero"
+import Gallery from '@/components/ui/gallery';
+import Cta from "@/components/cta"
 
 export default function Page() {
+    const images = [
+        {
+            src: "/img/haircuts/men/8.jpg",
+            alt: "Men's Haircut"
+        },
+        {
+            src: "/img/haircuts/men/2.jpg",
+            alt: "Men's Haircut"
+        },
+        {
+            src: "/img/haircuts/men/3.jpg",
+            alt: "Men's Haircut"
+        },
+        {
+            src: "/img/haircuts/men/5.jpg",
+            alt: "Men's Haircut"
+        },
+        {
+            src: "/img/haircuts/men/6.jpg",
+            alt: "Men's Haircut"
+        },
+        {
+            src: "/img/haircuts/men/7.jpg",
+            alt: "Men's Haircut"
+        },
+    ];
+
     return (
-        <Hero title="Shaving">
-            <p>A bit about my shaving service and what I have to offer with it.</p>
-        </Hero>
+        <>
+            <Hero title="Shaving">
+                <p></p>
+            </Hero>
+            
+            <Gallery title="Examples" className="shaving" galleryImages={images} />
+        </>
     )
 }
--- components/ui/gallery.tsx
+++ components/ui/gallery.tsx
@@ -28,7 +28,7 @@ export default function Gallery({ title, galleryImages, className }: GalleryProp
                 <div className="border-b border-gray-300 pb-2 mb-4 text-center">
                     <TitleMd>{title}</TitleMd>
                 </div>
-                <ul className="gallery-list grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                <ul className="gallery-list grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
                     {galleryImages.map((image, index) => (
                         <li key={index} className="cursor-pointer">
                             <Image
