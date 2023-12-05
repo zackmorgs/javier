360df8b8732b51305c8dc04e6d1f60897c81f5ef
zackmorgs <zackmorgenthaler@gmail.com>
2023-12-05 05:57:32 -0600

    worked on calendar

--- components/calendar/calendar-day.tsx
+++ components/calendar/calendar-day.tsx
@@ -3,19 +3,22 @@ interface CalendarDayProps {
     selected: boolean;
     [key: string]: any; // This line is for any additional props
     onClick?: () => void; // Optional onClick function
-    // Other props
+    enabled: boolean;
+    isToday: boolean;
 }
 
-export default function CalendarDay({ date, selected, onClick }: CalendarDayProps) {
+export default function CalendarDay({ date, selected, enabled, isToday, onClick }: CalendarDayProps) {
     if (!date) {
         return <div className="calendar-day null-date"></div>;
     } else {
         return (
             <div 
-                className={`calendar-day text-center ${selected ? "selected" : ""}`} 
+                className={`calendar-day${selected ? " selected" : ""}${enabled ? " enabled" : " disabled"}${isToday ? " today" : ""}`} 
                 onClick={onClick} // Add the onClick event handler
             >
-                {date.getDate().toString()}
+                <span className="date">
+                    {date.getDate().toString()}
+                </span>
             </div>
         );
     }
--- components/calendar/calendar.tsx
+++ components/calendar/calendar.tsx
@@ -9,20 +9,23 @@ import '@/styles/calendar.scss';
 
 const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 const months = [
-    'January', 'February', 'March', 'April', 'May', 'June', 
+    'January', 'February', 'March', 'April', 'May', 'June',
     'July', 'August', 'September', 'October', 'November', 'December'
 ];
 
 type CalendarState = {
     selectedDate: Date;
-    dates: (Date | null)[];
+    dates: Date;
+    today: Date;
 };
+
 export default class Calendar extends Component<{}, CalendarState> {
     constructor(props: {}) {
         super(props);
         this.state = {
             selectedDate: new Date(),
-            dates: []
+            dates: [],
+            today: new Date()
         };
     }
 
@@ -37,19 +40,45 @@ export default class Calendar extends Component<{}, CalendarState> {
     }
 
     daysInMonth = (date: Date): number => {
+        // creates a new date, with date passed. passing 0 to the date parameter will return the date before the next month which gives you the total days in the month.
         return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
     };
 
     getDates = (date: Date): (Date | null)[] => {
         const totalDays = this.daysInMonth(date);
-        const firstDay = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
-        const paddingDays = firstDay;
-        return [
-            ...Array(paddingDays).fill(null),
-            ...Array.from({ length: totalDays }, (_, i) => new Date(date.getFullYear(), date.getMonth(), i + 1)),
-        ];
-    };
+        const firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
+        const lastDayOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
 
+        // Calculate previous month's trailing dates
+        const prevMonth = new Date(date.getFullYear(), date.getMonth(), 0);
+        // get total days in month for last month
+        const prevMonthDays = this.daysInMonth(prevMonth);
+        // this i just dont get but it works. thanks chatgpt. we use it in the following line to create the array length.
+        const prevMonthTrailingDays = firstDayOfMonth;
+        // creates an array of length prevMonthTrailingDays 
+        const prevMonthDates = Array.from({ length: prevMonthTrailingDays }, (_, i) => {
+            // create a new date using prev month. date of prev month is calculated from prevMonthDays and is decremented incrementally (hope that makes sense :])
+            return new Date(prevMonth.getFullYear(), prevMonth.getMonth(), prevMonthDays - i);
+        }).reverse();
+
+        // Current month's dates
+        // uses from method to create an array of totalDays which gives total days in the month.
+        const currentMonthDates = Array.from({ length: totalDays }, (_, i) => {
+            // returns a date which date is incremented from the total length of days (using i)
+            return new Date(date.getFullYear(), date.getMonth(), i + 1);
+        });
+
+        // Calculate next month's leading dates
+        // i think the six means theres max of six days on the next month but im not sure.
+        const nextMonthLeadingDays = 6 - lastDayOfMonth;
+
+        // 
+        const nextMonthDates = Array.from({ length: nextMonthLeadingDays }, (_, i) => {
+            return new Date(date.getFullYear(), date.getMonth() + 1, i + 1);
+        });
+
+        return [...prevMonthDates, ...currentMonthDates, ...nextMonthDates];
+    };
     setDates = (date: Date): void => {
         const dates = this.getDates(date);
         this.setState({ dates });
@@ -66,27 +95,109 @@ export default class Calendar extends Component<{}, CalendarState> {
             selectedDate: new Date(this.state.selectedDate.getFullYear(), this.state.selectedDate.getMonth() - 1, 1)
         });
     };
-    handleDayClick = (date: Date) => (): void => {
-        this.setState({
-            selectedDate: date
-        })
+    handleDayClick = (date: Date) => {
+        return () => {
+            this.setState({
+                selectedDate: date
+            });
+        };
     };
+    isPreviousDay = (date: Date | null): boolean => {
+        if (!date) return false;
+
+        const today = this.state.today;
+        // Create a new date object for today at 00:00:00 for comparison
+        const startOfToday = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+
+        return date < startOfToday;
+    };
+
+    isSelectedDate = (date: Date | null): boolean => {
+        const { selectedDate } = this.state;
+
+        return date && selectedDate &&
+            date.getDate() === selectedDate.getDate() &&
+            date.getMonth() === selectedDate.getMonth() &&
+            date.getFullYear() === selectedDate.getFullYear();
+    };
+
+    isToday = (date: Date | null): boolean => {
+        const { today } = this.state;
+
+        return date &&
+            date.getDate() === today.getDate() &&
+            date.getMonth() === today.getMonth() &&
+            date.getFullYear() === today.getFullYear();
+    };
+    isSunday = (date: Date | null): boolean => {
+        return date ? date.getDay() === 0 : false;
+    };
+
+    isHoliday = (date: Date | null): boolean => {
+        if (!date) return false;
+
+        const holidays = [
+            { month: 0, day: 1 },   // Example: New Year's Day (January 1st)
+            { month: 11, day: 25 }, // Example: Christmas (December 25th)
+            // Add more holidays as needed
+        ];
+
+        return holidays.some(holiday =>
+            date.getDate() === holiday.day && date.getMonth() === holiday.month
+        );
+    };
+
+    isDayOff = (date: Date | null): boolean => {
+        return false;
+    }
+    isValidDateSelected = (): boolean => {
+        const { selectedDate } = this.state;
+        return selectedDate && 
+               !this.isPreviousDay(selectedDate) &&
+               !this.isSunday(selectedDate) &&
+               !this.isHoliday(selectedDate) &&
+               !this.isDayOff(selectedDate);
+    };
+    handleSelectDateClick = (): void => {
+        console.log("date selected:", this.state.selectedDate);
+    }
     render() {
-        const { selectedDate, dates } = this.state;
+
         return (
-            <div id="calendar" className="mb-4">
+            <div id="calendar" className="mb-4 h-screen md:h-auto">
                 <div className="text-center border-b mb-3 pb-2">
                     <TitleMd>
-                        <FiArrowLeft className="inline-block icon" onClick={this.handlePrevMonthClick} /> {months[selectedDate.getMonth()]} {selectedDate.getFullYear()} <FiArrowRight className="inline-block icon" onClick={this.handleNextMonthClick}/>
+                        <FiArrowLeft className="inline-block icon" onClick={this.handlePrevMonthClick} /> {months[this.state.selectedDate.getMonth()]} {this.state.selectedDate.getFullYear()} <FiArrowRight className="inline-block icon" onClick={this.handleNextMonthClick} />
                     </TitleMd>
+                    <small>
+                        Select a date from below.
+                    </small>
                 </div>
                 <div id="calendar-days" className="grid grid-cols-7 gap-1">
                     {daysOfWeek.map(day => (
-                        <div key={day} className="py-2 text-center">{day}</div>
+                        <div key={day} className="day-of-week">{day}</div>
                     ))}
-                    {dates.map((date, index) => (
-                        <CalendarDay key={index} date={date} selected={!date ? false : (date.getDate() === selectedDate.getDate())} onClick={this.handleDayClick(date)} />
+                    {this.state.dates.map((date, index) => (
+                        <CalendarDay
+                            key={index}
+                            date={date}
+                            selected={this.isSelectedDate(date)}
+                            onClick={date ? this.handleDayClick(date) : undefined}
+                            enabled={!this.isPreviousDay(date) && !this.isSunday(date) && !this.isHoliday(date) && !this.isDayOff(date)}
+                            isToday={this.isToday(date)}
+                        />
                     ))}
+                </div>
+                <div className="flex justify-center md:justify-start">
+                    <div className="mt-4">
+                        <button 
+                            id="select_date"
+                            className="btn btn-primary" 
+                            disabled={!this.isValidDateSelected()}
+                            onClick={this.handleSelectDateClick}>
+                            Select Date
+                        </button>
+                    </div>
                 </div>
             </div>
         );
