53a44b6533cb42a26eb41dc42c38154a94d8de10
zackmorgs <zackmorgenthaler@gmail.com>
2023-12-05 03:48:59 -0600

    turned calendar into class component, select date

--- components/calendar/calendar-day.tsx
+++ components/calendar/calendar-day.tsx
@@ -2,21 +2,21 @@ interface CalendarDayProps {
     date: Date;
     selected: boolean;
     [key: string]: any; // This line is for any additional props
+    onClick?: () => void; // Optional onClick function
+    // Other props
 }
-export default function CalendarDay(props : CalendarDayProps) {
-    console.log(props.date)
-    if (!props.date) {
-        return (
-            <div className="calendar-day null-date">
 
-            </div>
-        )
+export default function CalendarDay({ date, selected, onClick }: CalendarDayProps) {
+    if (!date) {
+        return <div className="calendar-day null-date"></div>;
     } else {
         return (
-            <div className={`calendar-day text-center${ props.selected ? " selected" : ""}`} key={props.key}>
-                {props.date.getDate().toString()}
+            <div 
+                className={`calendar-day text-center ${selected ? "selected" : ""}`} 
+                onClick={onClick} // Add the onClick event handler
+            >
+                {date.getDate().toString()}
             </div>
-        )
+        );
     }
-
 }
--- components/calendar/calendar.tsx
+++ components/calendar/calendar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import CalendarDay from "./calendar-day";
 import { TitleMd } from "../ui/title";
 import { FiArrowLeft, FiArrowRight } from "react-icons/fi";
@@ -7,60 +7,88 @@ import { select } from "@material-tailwind/react";
 
 import '@/styles/calendar.scss';
 
-export default function Calendar() {
-    const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-    const months = [
-        'January', 'February', 'March', 'April', 'May', 'June', 
-        'July', 'August', 'September', 'October', 'November', 'December'
-    ];
+const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const months = [
+    'January', 'February', 'March', 'April', 'May', 'June', 
+    'July', 'August', 'September', 'October', 'November', 'December'
+];
 
-    const [selectedDate, setSelectedDate] = useState(new Date());
+type CalendarState = {
+    selectedDate: Date;
+    dates: (Date | null)[];
+};
+export default class Calendar extends Component<{}, CalendarState> {
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            selectedDate: new Date(),
+            dates: []
+        };
+    }
 
-    const daysInMonth = (date: Date) => {
+    componentDidMount() {
+        this.setDates(this.state.selectedDate);
+    }
+
+    componentDidUpdate(prevProps: {}, prevState: CalendarState) {
+        if (prevState.selectedDate !== this.state.selectedDate) {
+            this.setDates(this.state.selectedDate);
+        }
+    }
+
+    daysInMonth = (date: Date): number => {
         return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
     };
 
-    const getDates = (date: Date) => {
-        const totalDays = daysInMonth(date);
+    getDates = (date: Date): (Date | null)[] => {
+        const totalDays = this.daysInMonth(date);
         const firstDay = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
         const paddingDays = firstDay;
         return [
             ...Array(paddingDays).fill(null),
-            ...Array.from({ length: totalDays }, (_, i) => {
-                return new Date(date.getFullYear(), date.getMonth(), i + 1);
-            }),
+            ...Array.from({ length: totalDays }, (_, i) => new Date(date.getFullYear(), date.getMonth(), i + 1)),
         ];
     };
 
-    const [dates, setDates] = useState(getDates(selectedDate));
-
-    useEffect(() => {
-        setDates(getDates(selectedDate));
-    }, [selectedDate]);
-
-    const handleNextMonthClick = () => {
-        setSelectedDate(new Date(selectedDate.getFullYear(), selectedDate.getMonth() + 1, 1));
+    setDates = (date: Date): void => {
+        const dates = this.getDates(date);
+        this.setState({ dates });
     };
 
-    const handlePrevMonthClick = () => {
-        setSelectedDate(new Date(selectedDate.getFullYear(), selectedDate.getMonth() - 1, 1));
+    handleNextMonthClick = (): void => {
+        this.setState({
+            selectedDate: new Date(this.state.selectedDate.getFullYear(), this.state.selectedDate.getMonth() + 1, 1)
+        });
     };
 
-    return (
-        <div id="calendar" >
-            <div className="text-center border-b mb-3 pb-2">
-                <TitleMd>
-                    <FiArrowLeft className="inline-block icon" onClick={handlePrevMonthClick} /> {months[selectedDate.getMonth()]} {selectedDate.getFullYear()}  <FiArrowRight className="inline-block icon" onClick={handleNextMonthClick}/>
-                </TitleMd>
+    handlePrevMonthClick = (): void => {
+        this.setState({
+            selectedDate: new Date(this.state.selectedDate.getFullYear(), this.state.selectedDate.getMonth() - 1, 1)
+        });
+    };
+    handleDayClick = (date: Date) => (): void => {
+        this.setState({
+            selectedDate: date
+        })
+    };
+    render() {
+        const { selectedDate, dates } = this.state;
+        return (
+            <div id="calendar" className="mb-4">
+                <div className="text-center border-b mb-3 pb-2">
+                    <TitleMd>
+                        <FiArrowLeft className="inline-block icon" onClick={this.handlePrevMonthClick} /> {months[selectedDate.getMonth()]} {selectedDate.getFullYear()} <FiArrowRight className="inline-block icon" onClick={this.handleNextMonthClick}/>
+                    </TitleMd>
+                </div>
+                <div id="calendar-days" className="grid grid-cols-7 gap-1">
+                    {daysOfWeek.map(day => (
+                        <div key={day} className="py-2 text-center">{day}</div>
+                    ))}
+                    {dates.map((date, index) => (
+                        <CalendarDay key={index} date={date} selected={!date ? false : (date.getDate() === selectedDate.getDate())} onClick={this.handleDayClick(date)} />
+                    ))}
+                </div>
             </div>
-            <div id="calendar-days" className="grid grid-cols-7 gap-1">
-                {daysOfWeek.map(day => (
-                    <div key={day} className="py-2 text-center">{day}</div>
-                ))}
-                {dates.map((date, index) => (
-                    <CalendarDay key={index} date={date} selected={!date? (false) : (date.getDate() == selectedDate.getDate())}/>
-                ))}
-            </div>
-        </div>
-    )
+        );
+    }
 }
